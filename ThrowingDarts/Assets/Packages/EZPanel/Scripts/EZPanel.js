b144139ab2a1bb64c66f387ebd34c13192422fd6
bennyp3333 <bennyp3333@gmail.com>
2025-02-09 16:46:13 -0600

    Add in EZPanel package and some shaders

---
+++ ThrowingDarts/Assets/Packages/EZPanel/Scripts/EZPanel.js
@@ -0,0 +1,47 @@
+//@input string panelType = "panel" {"widget":"combobox", "values":[{"label":"panel", "value":"panel"}, {"label":"pill", "value":"pill"}, {"label":"sphere", "value":"sphere"}]}
+//@ui {"widget":"separator"}
+//@input bool debug
+//@input string debugName = "EZPanel" {"showIf":"debug"}
+//@input Component.Text debugText {"showIf":"debug"}
+
+var self = script.getSceneObject();
+var selfTransform = self.getTransform();
+
+var panelRenderMesh;
+
+function init(){
+    panelRenderMesh = self.getComponent("Component.RenderMeshVisual");
+    //make material unique
+    debugPrint("Initilized!");
+}
+
+script.setSize = function(size, time){
+    
+}
+
+script.setColor = function(color, time){
+    
+}
+
+script.fade = function(inOut, time, recursive){
+    
+}
+
+script.createEvent("OnStartEvent").bind(init);
+
+// Debug
+function debugPrint(text){
+    if(script.debug){
+        var newLog = script.debugName + ": " + text;
+        if(global.textLogger){ global.logToScreen(newLog); }
+        if(script.debugText){ script.debugText.text = newLog; }
+        print(newLog);
+    }
+}
+
+function errorPrint(text){
+    var errorLog = "!!ERROR!! " + script.debugName + ": " + text;
+    if(global.textLogger){ global.logError(errorLog); }
+    if(script.debugText){ script.debugText.text = errorLog; }
+    print(errorLog);
+}
