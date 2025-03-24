cf35d31d4baf16931dea99ae8e8306499beab2ae
bennyp3333 <bennyp3333@gmail.com>
2025-03-24 11:39:45 -0500

    Update color utils

--- ThrowingDarts/Assets/Packages/Core/Utilities/ColorUtils.js
+++ ThrowingDarts/Assets/Packages/Core/Utilities/ColorUtils.js
@@ -6,40 +6,54 @@ function rgbToHsv(color) {
     var delta = max - min;
     
     var h = 0, s = 0, v = max;
-    
+
     if (delta > 0) {
         if (max === r) {
             h = ((g - b) / delta) % 6;
         } else if (max === g) {
-            h = (b - r) / delta + 2;
+            h = ((b - r) / delta) + 2;
         } else {
-            h = (r - g) / delta + 4;
+            h = ((r - g) / delta) + 4;
         }
-        
+
         h /= 6;
-        if (h < 0) h += 1; // Ensure hue is in range [0,1]
-        
-        s = max === 0 ? 0 : delta / max;
+        if (h < 0) h += 1; // Normalize to [0,1]
+    }
+
+    s = max === 0 ? 0 : delta / max;
+    
+    if (color.w !== undefined) {
+        return new vec4(h, s, v, color.a);
+    } else {
+        return new vec3(h, s, v);
     }
     
-    return { h: h, s: s, v: v };
 }
 
-function hsvToRgb(h, s, v) {
+function hsvToRgb(color) {
+    var h = color.x;
+    var s = color.y;
+    var v = color.z;
+
     var c = v * s;
-    var x = c * (1 - Math.abs((h * 6) % 2 - 1));
+    var hh = h * 6; // Range [0,6)
+    var x = c * (1 - Math.abs(hh % 2 - 1));
     var m = v - c;
-    
+
     var r = 0, g = 0, b = 0;
-    
-    if (h < 1/6) { r = c; g = x; b = 0; }
-    else if (h < 2/6) { r = x; g = c; b = 0; }
-    else if (h < 3/6) { r = 0; g = c; b = x; }
-    else if (h < 4/6) { r = 0; g = x; b = c; }
-    else if (h < 5/6) { r = x; g = 0; b = c; }
+
+    if (hh < 1) { r = c; g = x; b = 0; }
+    else if (hh < 2) { r = x; g = c; b = 0; }
+    else if (hh < 3) { r = 0; g = c; b = x; }
+    else if (hh < 4) { r = 0; g = x; b = c; }
+    else if (hh < 5) { r = x; g = 0; b = c; }
     else { r = c; g = 0; b = x; }
     
-    return new vec4(r + m, g + m, b + m, 1.0);
+    if (color.w !== undefined) {
+        return new vec4(r + m, g + m, b + m, color.a);
+    } else {
+        return new vec3(r + m, g + m, b + m);
+    }
 }
 
 function colorRandom(randomAlpha) {
